7b7a7be9c1342b6e793e83177273987033ff6b14
konapun <bremen.braun@gmail.com>
2020-07-02 13:26:50 -0500

    Install nvm

---
+++ install.zsh
@@ -0,0 +1,58 @@
+#!/usr/bin/env zsh
+# TODO:
+# - add update functionality
+# - add option to symlink rather than copy
+
+# INSTALLATION
+apt_packages=(fzf htop neofetch neovim nodejs ranger ripgrep tmux yarn)
+brew_packages=() # TODO
+brew_cask_packages=() # TODO
+
+if [[ $OSTYPE == linux-gnu ]]; then
+  apt install $apt_packages
+elif [[ $OSTYPE == darwin ]]; then
+  brew install $brew_packages
+  brew cask install $brew_cask_packages
+fi
+
+function install_oh_my_zsh {
+  if [[ ! -d ~/.oh-my-zsh ]]; then
+    sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  fi
+}
+
+function install_nvm {
+  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+}
+
+function configure_git {
+  cp git/gitconfig ~/.gitconfig
+}
+
+function configure_neovim {
+  mkdir -p ~/.config
+  cp -r ./nvim ~/.config
+}
+
+function configure_tmux {
+  cp ./tmux/tmux.conf ~/.tmux.conf
+}
+
+function configure_zsh {
+  cp ./zsh/ignore ~/.ignore
+  cp ./zsh/zshenv ~/.zshenv
+  cp ./zsh/zshrc ~/.zshrc
+  cp -r ./zsh/custom ~/.oh-my-zsh
+}
+
+# CUSTOM INSTALLATION
+install_oh_my_zsh
+install_nvm
+
+# CONFIGURATION
+configure_zsh
+configure_git
+configure_tmux
+configure_neovim
+
+
