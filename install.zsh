3d7ebfd48b0c9c4e13aef5cb56f8e3fd8cbf5dcb
konapun <bremen.braun@gmail.com>
2020-07-06 20:44:43 -0500

    update airline config

--- install.zsh
+++ install.zsh
@@ -8,6 +8,8 @@ apt_packages=(ctags fzf htop neofetch neovim nodejs ranger ripgrep tmux yarn)
 brew_packages=(ctags fzf htop neofetch neovim node ranger ripgrep tmux yarn)
 brew_cask_packages=(amethyst font-cascadia-pl font-ibm-plex dash gimp iterm2 rectangle slack spotify ultimaker-cura vanilla)
 
+targets=$@
+
 if [[ $OSTYPE == linux-gnu ]]; then
   apt install $apt_packages
 elif [[ $OSTYPE == darwin ]]; then
