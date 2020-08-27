9edfa77a8f265a716bf0605240958fbf28c06e24
konapun <bremen.braun@gmail.com>
2020-08-27 10:44:08 -0500

    update zsh plugins

--- zsh/configure.zsh
+++ zsh/configure.zsh
@@ -2,6 +2,12 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
   sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+if [[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+elif [[ $UPDATE ]]; then
+  git -C ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting pull
+fi
+
 local zsh_files=(ignore zshenv zshrc p10k.zsh)
 for zsh_file in $zsh_files; do
   local syspath=~/.$zsh_file
