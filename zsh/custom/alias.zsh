fd7c62e6b8c343f99be9129bd38b7d2dcf5c52b0
konapun <bremen.braun@gmail.com>
2020-09-16 09:32:21 -0500

    check existence before assigning alias

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -2,8 +2,8 @@ alias ..="cd .."
 alias ...="cd ../.."
 
 alias vim=nvim
-alias bat=batcat
 
+command -v batcat &> /dev/null && alias bat=batcat
 command -v bat &> /dev/null && alias cat=bat
 command -v lsd &> /dev/null && alias ls=lsd
 command -v thefuck &> /dev/null && eval $(thefuck --alias)
