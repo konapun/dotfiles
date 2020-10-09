bbd9d7f683f5e3d7d63fbf239b868b8fad11d9eb
konapun <bremen.braun@gmail.com>
2020-10-08 22:43:27 -0500

    themable segments

--- zsh/custom/tmux.zsh
+++ zsh/custom/tmux.zsh
@@ -1,7 +1,7 @@
-export TMUX_STATUSBAR_BG=colour0
-export TMUX_LEFT_SEGMENT_START=
-export TMUX_LEFT_SEGMENT_END=
-export TMUX_LEFT_SEPARATOR=
-export TMUX_RIGHT_SEGMENT_START=
-export TMUX_RIGHT_SEGMENT_END=
-export TMUX_RIGHT_SEPARATOR=
+export TMUX_STATUSBAR_BG={{background}}
+export TMUX_LEFT_SEGMENT_START={{left_segment_start}}
+export TMUX_LEFT_SEGMENT_END={{left_segment_end}}
+export TMUX_LEFT_SEPARATOR={{left_separator}}
+export TMUX_RIGHT_SEGMENT_START={{right_segment_start}}
+export TMUX_RIGHT_SEGMENT_END={{right_segment_end}}
+export TMUX_RIGHT_SEPARATOR={{right_separator}}
