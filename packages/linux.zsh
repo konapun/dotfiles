86c1aa35f9d4f89fc62f8e38e9747563741fc878
konapun <bremen.braun@gmail.com>
2020-11-04 22:29:22 -0600

    add tree

--- packages/linux.zsh
+++ packages/linux.zsh
@@ -1,7 +1,7 @@
 GRAPHICAL_PACKAGE_MANAGER="sudo apt"
 PACKAGE_MANAGER="sudo apt"
 
-CORE_PACKAGES=(bat clojure ctags command-not-found fzf highlight htop leiningen neofetch neovim nodejs python3 python3-pip ranger ripgrep thefuck tmux yarn zsh) # stow navi ncmpcpp sxhkd
+CORE_PACKAGES=(bat clojure ctags command-not-found fzf highlight htop leiningen neofetch neovim nodejs python3 python3-pip ranger ripgrep thefuck tmux tree yarn zsh) # stow navi ncmpcpp sxhkd
 GRAPHICAL_PACKAGES=(audacity blender gimp lmms slack spotify-client steam)
 
 # lsd isn't currently available in apt so install it from releases
