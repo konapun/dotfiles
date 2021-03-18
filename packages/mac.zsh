79d1eb102e8f98d5eda094cb0cace56ed80828dc
konapun <bremen.braun@gmail.com>
2021-03-18 14:46:33 -0500

    add docker mac package and source bash_profile if exists

--- packages/mac.zsh
+++ packages/mac.zsh
@@ -1,6 +1,6 @@
 PACKAGE_MANAGER=brew
 GRAPHICAL_PACKAGE_MANAGER="brew cask"
 
-CORE_PACKAGES=(babashka bat calcurse ctags fzf gron gsed highlight htop hub lsd navi neofetch neovim node ranger ripgrep rust thefuck tmux) # stow
+CORE_PACKAGES=(babashka bat calcurse ctags docker fzf gron gsed highlight htop hub lsd navi neofetch neovim node ranger ripgrep rust thefuck tmux) # stow
 GRAPHICAL_PACKAGES=(alacritty amethyst edex-ui font-cascadia-pl font-ibm-plex dash gimp iterm2 rectangle slack spotify ultimaker-cura vanilla) # TODO further separate these out into categories
 
