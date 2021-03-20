7bc47f1debf5d181114a4a4848f063e546b7990c
konapun <bremen.braun@gmail.com>
2021-03-20 17:43:52 -0500

    add missing packages for iSH

--- packages/linux.zsh
+++ packages/linux.zsh
@@ -1,9 +1,9 @@
 if command -v pkg &> /dev/null; then # termux
-  source ./packages/linux/pkg.zsh
+  source ./packages/linux/package_manager/pkg.zsh
 elif command -v apt &> /dev/null; then # debian, ubuntu, etc
-  source ./packages/linux/apt.zsh
+  source ./packages/linux/package_manager/apt.zsh
 elif command -v apk &> /dev/null; then # iSH
-  source ./packages/linux/apk.zsh
+  source ./packages/linux/package_manager/apk.zsh
 else
   warn "Unable to detect package manager (currently, apt and pkg are supported)"
   exit 1
