6f067c3cd8f27c889773fe563354001fdcde93ac
konapun <bremen.braun@gmail.com>
2021-07-02 22:51:38 -0500

    add linux installer for neovim 0.5

---
+++ packages/definitions/neovim-0.5.zsh
@@ -0,0 +1,13 @@
+# Even though 0.5.x is the current stable version apt and the like are still on 0.4.x
+# This script installs the current stable version as an app image
+
+local dest=$HOME/.local/bin
+
+if [[ $IS_LINUX ]]; then
+  local src=https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+
+  wget -P $dest $src
+  chmod u+x $dest/nvim.appimage
+  mv $dest/nvim.appimage $dest/nvim
+fi
+
