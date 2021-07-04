9677cf293dc2d54e43f8c7cc40770443c7c874a2
konapun <bremen.braun@gmail.com>
2021-07-03 23:54:20 -0500

    Updated plugins for 0.5

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+local cmd = vim.cmd
+local opt = vim.opt
+
+require('plugins')
+require('keymappings')
+require('config')
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,134 @@
+-- Bootstrap packer
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- allow packer to manage itself
+
+  -- themes
+  use {'npxbr/gruvbox.nvim', requires = {'rktjmp/lush.nvim'}}               -- Lua port of the most famous vim colorscheme
+  -- use 'dylanaraps/wal.vim'                                               -- A colorscheme that adapts to your terminal colors
+  -- use 'morhetz/gruvbox'                                                  -- Retro groove color scheme for Vim
+  -- use 'franbach/miramare'                                                -- Comfortable & Pleasant Color Scheme for Vim
+  -- use 'sainnhe/forest-night'                                             -- Comfortable & Pleasant Color Scheme for Vim
+  -- use 'sainnhe/gruvbox-material'                                         -- Gruvbox with Material Palette
+  -- use 'fcpg/vim-farout'                                                  -- Dark retro base16 theme for gvim and 24-bit color terminals
+  -- use 'fcpg/vim-fahrenheit'                                              -- Dark, warm base16 Vim theme for 256-color terminals
+  -- use 'branwright1/salvation-vim'                                        -- Dark colorscheme for Vim/Neovim.
+
+  -- general
+  use {'nvim-telescope/telescope.nvim', requires = {                     -- Find, Filter, Preview, Pick (Lua)
+    {'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}
+  }}
+  use {'mg979/vim-visual-multi', branch = 'master'}                      -- Multiple cursors plugin for vim/neovim
+  use 'hrsh7th/nvim-compe'                                               -- Nvim code autocompletion (Lua)
+  use 'nvim-treesitter/nvim-treesitter'                                  -- Nvim Treesitter configurations and abstraction layer (Lua)
+  use 'kevinhwang91/rnvimr'                                              -- Use Ranger in a floating window via RPC (Lua)
+  use 'vim-airline/vim-airline'                                          -- Statusline TODO: replace with Lua statusline
+  use 'w0rp/ale'                                                         -- Asynchronous linting
+  use 'mhartington/formatter.nvim'                                       -- Format runner (Lua)
+  use 'ludovicchabant/vim-gutentags'                                     -- Tag file management
+  use 'editorconfig/editorconfig-vim'                                    -- Support for .editorconfig
+  use 'kshenoy/vim-signature'                                            -- Toggle, display and navigate marks
+  use 'vim-scripts/ReplaceWithRegister'                                  -- Replace text object with contents of register
+  use "tversteeg/registers.nvim"                                         -- Preview the contents of registers (Lua)
+  use 'sirver/ultisnips'                                                 -- The ultimate snippet solution for Vim
+  use 'numtostr/FTerm.nvim'                                              -- Nvim floating terminals (Lua)
+  use 'norcalli/nvim-colorizer.lua'                                      -- Fast Nvim colorizer (Lua)
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'                                            -- Quickstart configurations for the Nvim LSP client (Lua)
+  use 'kabouzeid/nvim-lspinstall'                                        -- Easily install LSP language servers
+  use 'onsails/lspkind-nvim'                                             -- VSCode-like pictograms for Nvim lsp completion items (Lua)
+  use 'simrat39/symbols-outline.nvim'                                    -- Tree like view for LSP symbols (Lua)
+  -- TODO: installers
+  -- TODO: https://github.com/nvim-lua/lsp-status.nvim ??
+  -- TODO: https://github.com/glepnir/lspsaga.nvim if telesope doesn't provide this functionality
+  -- TODO: https://github.com/RishabhRD/nvim-lsputils ""
+  -- TODO: https://github.com/ray-x/lsp_signature.nvim if normal completion doesn't show this info
+  -- TODO: https://github.com/ray-x/navigator.lua if telescope doesn't provide this functionality
+  -- TODO: https://github.com/folke/trouble.nvim ""
+
+  -- editing
+  use 'windwp/nvim-autopairs'                                            -- Pair autocompletion (Lua)
+  use 'b3nj5m1n/kommentary'                                              -- Nvim commenting plugin (Lua)
+  use 'blackCauldron7/surround.nvim'                                     -- Add/delete/replace surroundings of a sandwiched text object (Lua)
+  use 'tpope/vim-unimpaired'                                             -- Pairs of handy bracket mappings use 'mbbill/undotree'                                                  -- Undo history visualizer
+  use 'ggandor/lightspeed.nvim'                                          -- Navigate anywhere in the current buffer in as few keystrokes as possible (Lua)
+  use 'tpope/vim-repeat'                                                 -- Enable repeating supported plugin maps
+  use 'mattn/emmet-vim'                                                  -- HTML/CSS text expansion
+
+  -- buffer/window
+  use 'camspiers/lens.vim'                                               -- Automatic window resizing
+  use 'qpkorr/vim-bufkill'                                               -- Interact with buffers without affecting window layout (TODO: Replace with Lua https://github.com/famiu/bufdelete.nvim eventually)
+  use 'simeji/winresizer'                                                -- Easy window resizing
+  use 't9md/vim-choosewin'                                               -- Land on window you chose like tmux's 'display-pane'
+  use 'farmergreg/vim-lastplace'                                         -- Intelligently reopen files at your last edit position
+  use 'tpope/vim-sleuth'                                                 -- Heuristically set buffer options
+  -- https://github.com/matbme/JABS.nvim???????? TODO can telescope provide this functionality?
+
+  -- database
+  use 'tpope/vim-dadbod'                                                 -- Modern database interface for vim
+
+  -- markup
+  use 'windwp/nvim-ts-autotag'                                           -- Use treesitter to auto close and auto rename HTML tags (Lua)
+
+  -- testing / debugging
+  use 'radenling/vim-dispatch-neovim'                                    -- Support for neovim's terminal emulator and job control to dispatch.vim
+  use 'tpope/vim-dispatch'                                               -- Asynchronous build and test dispatcher
+  use {'rcarriga/nvim-dap-ui', requires = {'mfussenegger/nvim-dap'}}     -- Nvim UI for Debug Adapter Protocol (Lua)
+  use {'rcarriga/vim-ultest',                                            -- The ultimate testing plugin for Nvim (Lua)
+    requires = {'vim-test/vim-test'},
+    run = ":UpdateRemotePlugins"
+  }
+
+  -- text objects
+  use 'nvim-treesitter/nvim-treesitter-textobjects'                      -- Create your own text objects using tree-sitter queries (Lua) TODO: Replace below with this
+  use 'kana/vim-textobj-entire'                                          -- Text objects for entire buffers
+  use 'kana/vim-textobj-indent'                                          -- Text objects for indented blocks of lines
+  use 'kana/vim-textobj-line'                                            -- Text objects for the current line
+  use 'kana/vim-textobj-user'                                            -- Create your own text objects
+
+  -- git
+  use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}  -- Git diff decoration, hunk navigation, and blame (Lua)
+  use 'sindrets/diffview.nvim'                                           -- Easily review all changed files for a git revision (Lua)
+  use 'tpope/vim-fugitive'                                               -- Git wrapper
+  use 'tpope/vim-rhubarb'                                                -- GitHub extension for fugitive.vim
+  -- TODO: https://github.com/TimUntersberger/neogit
+
+  -- decoration/layout
+  use 'camspiers/animate.vim'                                            -- Window animation
+  use 'sunjon/shade.nvim'                                                -- Dim inactive windows (Lua)
+  use 'junegunn/goyo.vim'                                                -- Distraction-free writing in Vim
+  use 'junegunn/limelight.vim'                                           -- Highlight current block
+  use 'yamatsum/nvim-cursorline'                                         -- Highlight matching words/lines under the cursor (Lua)
+  use 'karb94/neoscroll.nvim'                                            -- Smooth scrolling (Lua)
+  use {                                                                  -- Icon set using nonicons for Nvim plugins and settings (Lua)
+    'yamatsum/nvim-nonicons',
+    requires = {'kyazdani42/nvim-web-devicons'}
+  }
+  -- use "lukas-reineke/indent-blankline.nvim"                              -- Indent guides for Nvim (Lua)
+
+  -- utilities
+  use 'vhyrro/neorg'                                                     -- Org mode for Nvim (Lua)
+  -- use {'AckslD/nvim-whichkey-setup.lua', requires = {                    -- Easy setup for which-key, a plugin to display possible keybindings of typed command (Lua)
+  --   'liuchengxu/vim-which-key'
+  -- }}
+  -- https://github.com/windwp/nvim-spectre TODO: search panel if telescope doesn't provide something similar
+
+  -- lisp/clojure
+  -- use 'guns/vim-sexp'                                                    -- Precision editing for S-expressions
+  -- use 'tpope/vim-sexp-mappings-for-regular-people'                       -- Better sexp mappings
+  -- use 'Olical/conjure'                                                   -- Interactive evaluation for Neovim (Clojure, Fennel, Janet)
+  -- use 'clojure-vim/vim-jack-in'                                          -- cider-jack-in for vim
+
+  -- other languages
+  use 'alunny/pegjs-vim'                                                 -- Syntax highlighting for PEG.js grammars
+end)
