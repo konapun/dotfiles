9edb04d2c88dd8f3df8f17a24f4a0e196da20804
konapun <bremen.braun@gmail.com>
2021-07-19 22:36:42 -0500

    more galaxyline config

--- nvim/lua/config/galaxyline.lua
+++ nvim/lua/config/galaxyline.lua
@@ -4,8 +4,32 @@ local gls = gl.section
 gl.short_line_list = {'LuaTree','vista','dbui'}
 
 local separator = {
-  left = '',
-  right = ''
+  left = '',
+  left_segment = '',
+  right = '',
+  right_segment = ''
+}
+local mode_map = {
+  n = {text = 'NORMAL', background = palette.purple, foreground = palette.background},
+  i = {text = 'INSERT', background = palette.yellow, foreground = palette.background},
+  c = {text = 'COMMAND', background = palette.red, foreground = palette.background},
+  v = {text = 'VISUAL', background = palette.green, foreground = palette.background},
+  V = {text = 'VISUAL LINE', background = palette.blue, foreground = palette.background},
+  ['^V'] = {text = 'VISUAL BLOCK', background = palette.cyan, foreground = palette.background},
+  t = {text = 'TERMINAL', background = palette.blue, foreground = palette.background},
+  --[[ no = palette.blue,
+  s = palette.orange,
+  S = palette.orange,
+  [""] = palette.orange,
+  ic = palette.yellow,
+  R = palette.red,
+  Rv = palette.red,
+  cv = palette.blue,
+  ce = palette.blue,
+  r = palette.cyan,
+  rm = palette.cyan,
+  ["r?"] = palette.cyan,
+  ["!"] = palette.blue, ]]
 }
 
 local buffer_not_empty = function()
@@ -18,45 +42,56 @@ end
 gls.left[1] = {
   FirstElement = {
     provider = function() return '▋' end,
-    highlight = {palette.green, palette.bright_green}
+    highlight = {palette.green, palette.purple}
   },
 }
-gls.left[2] = {
+--[[ gls.left[2] = {
   ViMode = {
     provider = function()
-      local alias = {n = 'NORMAL',i = 'INSERT',c= 'COMMAND',v= 'VISUAL',V= 'VISUAL LINE', [''] = 'VISUAL BLOCK'}
-      return alias[vim.fn.mode()]
+      return mode_map[vim.fn.mode()].text
     end,
     separator = separator.left,
     separator_highlight = {palette.purple,function()
       if not buffer_not_empty() then
         return palette.purple
       end
-      return palette.blue
+      return palette.green -- next segment
     end},
     highlight = {palette.background,palette.purple,'bold'},
   },
+} ]]
+gls.left[2] = {
+  ViMode = {
+    provider = function()
+      local mode_config = mode_map[vim.fn.mode()]
+      -- auto change color according the vim mode
+      vim.api.nvim_command("hi GalaxyViMode guibg=" .. mode_config.background)
+      return mode_config.text
+    end,
+    separator_highlight = { "NONE", palette.background },
+    highlight = { "NONE", palette.background },
+  }
 }
 gls.left[3] ={
   FileIcon = {
     provider = 'FileIcon',
     condition = buffer_not_empty,
-    highlight = {require('galaxyline.provider_fileinfo').get_file_icon_color,palette.blue},
+    highlight = {require('galaxyline.provider_fileinfo').get_file_icon_color,palette.green},
   },
 }
 gls.left[4] = {
   FileName = {
     provider = {'FileName','FileSize'},
     condition = buffer_not_empty,
-    separator = separator.right,
-    separator_highlight = {palette.purple,palette.blue},
-    highlight = {palette.red,palette.blue}
+    separator = separator.left,
+    separator_highlight = {palette.green,palette.purple},
+    highlight = {palette.background,palette.green}
   }
 }
 
 gls.left[5] = {
   GitIcon = {
-    provider = function() return '  ' end,
+    provider = function() return ' ' end,
     condition = buffer_not_empty,
     highlight = {palette.bright_yellow,palette.purple},
   }
@@ -121,7 +156,7 @@ gls.right[1]= {
 gls.right[2] = {
   LineInfo = {
     provider = 'LineColumn',
-    separator = ' | ',
+    separator = separator.right_segment,
     separator_highlight = {palette.blue,palette.purple},
     highlight = {palette.background,palette.purple},
   },
@@ -129,8 +164,8 @@ gls.right[2] = {
 gls.right[3] = {
   PerCent = {
     provider = 'LinePercent',
-    separator = separator.left,
-    separator_highlight = {palette.purple,palette.blue},
+    separator = separator.right,
+    separator_highlight = {palette.blue,palette.purple},
     highlight = {palette.background,palette.blue},
   }
 }
