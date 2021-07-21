c80e3a9691de5f6643b237687ff578c661ef2e3f
konapun <bremen.braun@gmail.com>
2021-07-21 18:42:56 -0500

    lua theme variables

--- nvim/configure.zsh
+++ nvim/configure.zsh
@@ -1,5 +1,5 @@
 NVIM_CONFIG_FILE=~/.config/nvim/init.vim
-NVIM_PALETTE_FILE=~/.config/nvim/lua/theme.lua
+NVIM_THEME_FILE=~/.config/nvim/lua/theme.lua
 NVIM_THEME_FILE=~/.config/nvim/colors/system.vim
 
 cp -r ./nvim ~/.config
@@ -17,30 +17,30 @@ themer_substitute background $BACKGROUND_TYPE $NVIM_CONFIG_FILE
 themer_cleanup $NVIM_CONFIG_FILE
 
 # Lua Palette Theming
-themer_substitute background $BACKGROUND $NVIM_PALETTE_FILE
-themer_substitute foreground $FOREGROUND $NVIM_PALETTE_FILE
-themer_substitute accent $ACCENT $NVIM_PALETTE_FILE
-themer_substitute black $BLACK $NVIM_PALETTE_FILE
-themer_substitute red $RED $NVIM_PALETTE_FILE
-themer_substitute green $GREEN $NVIM_PALETTE_FILE
-themer_substitute yellow $YELLOW $NVIM_PALETTE_FILE
-themer_substitute blue $BLUE $NVIM_PALETTE_FILE
-themer_substitute purple $PURPLE $NVIM_PALETTE_FILE
-themer_substitute cyan $CYAN $NVIM_PALETTE_FILE
-themer_substitute white $WHITE $NVIM_PALETTE_FILE
-themer_substitute bright_black $BRIGHT_BLACK $NVIM_PALETTE_FILE
-themer_substitute bright_red $BRIGHT_RED $NVIM_PALETTE_FILE
-themer_substitute bright_green $BRIGHT_GREEN $NVIM_PALETTE_FILE
-themer_substitute bright_yellow $BRIGHT_YELLOW $NVIM_PALETTE_FILE
-themer_substitute bright_blue $BRIGHT_BLUE $NVIM_PALETTE_FILE
-themer_substitute bright_purple $BRIGHT_PURPLE $NVIM_PALETTE_FILE
-themer_substitute bright_cyan $BRIGHT_CYAN $NVIM_PALETTE_FILE
-themer_substitute bright_white $BRIGHT_WHITE $NVIM_PALETTE_FILE
-themer_substitute left_separator $LEFT_SEPARATOR $NVIM_PALETTE_FILE
-themer_substitute right_separator $RIGHT_SEPARATOR $NVIM_PALETTE_FILE
-themer_substitute left_segment_separator $LEFT_SEGMENT_START $NVIM_PALETTE_FILE
-themer_substitute right_segment_separator $RIGHT_SEGMENT_START $NVIM_PALETTE_FILE
-themer_cleanup $NVIM_PALETTE_FILE
+themer_substitute background $BACKGROUND $NVIM_THEME_FILE
+themer_substitute foreground $FOREGROUND $NVIM_THEME_FILE
+themer_substitute accent $ACCENT $NVIM_THEME_FILE
+themer_substitute black $BLACK $NVIM_THEME_FILE
+themer_substitute red $RED $NVIM_THEME_FILE
+themer_substitute green $GREEN $NVIM_THEME_FILE
+themer_substitute yellow $YELLOW $NVIM_THEME_FILE
+themer_substitute blue $BLUE $NVIM_THEME_FILE
+themer_substitute purple $PURPLE $NVIM_THEME_FILE
+themer_substitute cyan $CYAN $NVIM_THEME_FILE
+themer_substitute white $WHITE $NVIM_THEME_FILE
+themer_substitute bright_black $BRIGHT_BLACK $NVIM_THEME_FILE
+themer_substitute bright_red $BRIGHT_RED $NVIM_THEME_FILE
+themer_substitute bright_green $BRIGHT_GREEN $NVIM_THEME_FILE
+themer_substitute bright_yellow $BRIGHT_YELLOW $NVIM_THEME_FILE
+themer_substitute bright_blue $BRIGHT_BLUE $NVIM_THEME_FILE
+themer_substitute bright_purple $BRIGHT_PURPLE $NVIM_THEME_FILE
+themer_substitute bright_cyan $BRIGHT_CYAN $NVIM_THEME_FILE
+themer_substitute bright_white $BRIGHT_WHITE $NVIM_THEME_FILE
+themer_substitute left_separator $LEFT_SEPARATOR $NVIM_THEME_FILE
+themer_substitute right_separator $RIGHT_SEPARATOR $NVIM_THEME_FILE
+themer_substitute left_segment_separator $LEFT_SEGMENT_START $NVIM_THEME_FILE
+themer_substitute right_segment_separator $RIGHT_SEGMENT_START $NVIM_THEME_FILE
+themer_cleanup $NVIM_THEME_FILE
 
 themer_substitute background $BACKGROUND $NVIM_THEME_FILE
 themer_substitute foreground $FOREGROUND $NVIM_THEME_FILE
