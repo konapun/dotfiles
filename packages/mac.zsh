a5a2f3f2a2ceab966e38402a9ef582927d7824be
konapun <bremen.braun@gmail.com>
2021-08-01 13:16:16 -0500

    add luarocks

--- packages/mac.zsh
+++ packages/mac.zsh
@@ -2,7 +2,7 @@ PACKAGE_MANAGER=brew
 GRAPHICAL_PACKAGE_MANAGER="brew --cask"
 INSTALL_COMMAND="install"
 
-CORE_PACKAGES=(babashka bat calcurse ctags docker fortune fzf golang gron gsed highlight htop hub lsd navi neofetch neovim node ripgrep rust thefuck tmux) # stow
+CORE_PACKAGES=(babashka bat calcurse ctags docker fortune fzf golang gron gsed highlight htop hub luarocks lsd navi neofetch neovim node ripgrep rust thefuck tmux) # stow
 GRAPHICAL_PACKAGES=(alacritty amethyst edex-ui font-cascadia-pl font-ibm-plex dash gimp iterm2 rectangle slack spotify ultimaker-cura vanilla) # TODO further separate these out into categories
 
 # non-brew packages
