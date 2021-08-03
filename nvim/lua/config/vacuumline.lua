e1405385ac528ba975183cc2bdb7007506d44ce1
konapun <bremen.braun@gmail.com>
2021-08-03 16:15:49 -0500

    vacuumline theming

--- nvim/lua/config/vacuumline.lua
+++ nvim/lua/config/vacuumline.lua
@@ -1,1 +1,15 @@
-require('vacuumline').setup({})
+require('vacuumline').setup({
+  theme = require('vacuumline.theme.gruvbox'),
+  --[[ segment = {
+    mode = {
+      map = {
+        n = {label = 'NORMAL', background = '#012383'}, -- NORMAL
+        i = {label = 'INSERT', background = '#847a32'}, -- INSERT
+        c = {label = ' ', background = '#18472a'}, -- COMMAND
+        v = {label = ' ', background = '#abe932'}, -- VISUAL
+        V = {label = ' ', background = '#4bc829'}, -- VISUAL LINE
+        t = {label = ' ', background = '#98989c'}, -- TERMINAL
+      }
+    }
+  } ]]
+})
--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,7 +1,6 @@
 local M = {}
 
--- Palette template set by dotfile installer and used by config
-local palette = {
+M.palette = {
   background = '{{background}}',
   foreground = '{{foreground}}',
   accent     = '{{accent}}',
@@ -23,44 +22,11 @@ local palette = {
   bright_white  = '{{bright_white}}'
 }
 
-M.palette = palette
-M.modes = {
-  n      = {text = ' ', background = palette.purple}, -- NORMAL
-  i      = {text = ' ', background = palette.green}, -- INSERT
-  c      = {text = ' ', background = palette.blue}, -- COMMAND
-  v      = {text = ' ', background = palette.yellow}, -- VISUAL
-  V      = {text = ' ', background = palette.bright_yellow}, -- VISUAL LINE
-  ['^V'] = {text = ' ', background = palette.cyan}, -- VISUAL BLOCK
-  t      = {text = ' ', background = palette.bright_purple}, -- TERMINAL
-  -- TODO
-  no     = {text = 'no', background = palette.bright_yellow},
-  s      = {text = 's', background = palette.bright_red},
-  S      = {text = 'S', background = palette.bright_green},
-  [''] = {text = '^S', background = palette.bright_blue},
-  ic     = {text = 'ic', background = palette.bright_cyan},
-  R      = {text = 'R', background = palette.purple},
-  Rv     = {text = 'Rv', background = palette.purple},
-  cv     = {text = 'cv', background = palette.purple},
-  ce     = {text = 'ce', background = palette.purple},
-  r      = {text = 'r', background = palette.purple},
-  rm     = {text = 'rm', background = palette.purple},
-  ['r?'] = {text = 'r?', background = palette.purple},
-  ['!']  = {text = '!', background = palette.purple}
-}
-
-M.icon = {
-  primary    = '▋', -- {{icon_primary|▋}}',
-  git        = '', -- {{icon_git|}}',
-  diff_add    = '', -- {{icon_gitadd|}}', -- 
-  diff_modify = '', -- {{icon_gitmodify|}}',
-  diff_remove = '', -- {{icon_gitremove|}}' -- 
-}
-
 M.separator = {
   left = '{{left_separator}}',
-  left_segment = '{{left_segment_separator}}',
+  left_section = '{{left_segment_separator}}',
   right = '{{right_separator}}',
-  right_segment = '{{right_segment_separator}}'
+  right_section = '{{right_segment_separator}}'
 }
 
 -- 
