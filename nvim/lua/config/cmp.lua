c6b1914dfb5bb99132e368e2fdffbaf73ad9515a
konapun <bremen.braun@gmail.com>
2021-11-02 22:46:09 -0500

    updates for deprecation warning

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -19,6 +19,10 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true
+    }),
     ['<Tab>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
