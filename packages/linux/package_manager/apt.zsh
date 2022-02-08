1b9d0e7ec46bf90aa044098e0080755d3f72fa64
konapun <bremen.braun@gmail.com>
2022-02-07 18:00:18 -0600

    add guile

--- packages/linux/package_manager/apt.zsh
+++ packages/linux/package_manager/apt.zsh
@@ -2,7 +2,7 @@ GRAPHICAL_PACKAGE_MANAGER="sudo apt"
 PACKAGE_MANAGER="sudo apt"
 INSTALL_COMMAND="install"
 
-CORE_PACKAGES=(bat calcurse cargo clojure command-not-found docker docker-compose fortune-mod fzf gh golang gron highlight htop hub leiningen luarocks neofetch nodejs python3 python3-pip ripgrep thefuck tmux universal-ctags tree zsh) # stow navi ncmpcpp sxhkd
+CORE_PACKAGES=(bat calcurse cargo clojure command-not-found docker docker-compose fortune-mod fzf gh golang gron guile highlight htop hub leiningen luarocks neofetch nodejs python3 python3-pip ripgrep thefuck tmux universal-ctags tree zsh) # stow navi ncmpcpp sxhkd
 GRAPHICAL_PACKAGES=(audacity blender gimp lmms milkytracker slack spotify-client steam)
 
 # run migrations
@@ -10,7 +10,6 @@ source ./packages/linux/package_manager/apt/migrations.zsh
 
 # packages which aren't currently available in apt
 source ./packages/definitions/lsd.zsh
-source ./packages/definitions/babashka.zsh
 source ./packages/definitions/oblique-fortunes.zsh
 source ./packages/definitions/neovim-0.5.zsh
 
--- packages/mac.zsh
+++ packages/mac.zsh
@@ -2,7 +2,7 @@ PACKAGE_MANAGER=brew
 GRAPHICAL_PACKAGE_MANAGER="brew --cask"
 INSTALL_COMMAND="install"
 
-CORE_PACKAGES=(bat calcurse ctags docker fortune fzf gh golang gron gsed highlight htop hub luarocks lsd navi neofetch neovim node ripgrep rust thefuck tmux) # babashka stow
+CORE_PACKAGES=(bat calcurse ctags docker fortune fzf gh golang gron gsed guile highlight htop hub luarocks lsd navi neofetch neovim node ripgrep rust thefuck tmux) # babashka stow
 GRAPHICAL_PACKAGES=(alacritty amethyst edex-ui font-cascadia-pl font-ibm-plex dash gimp iterm2 rectangle slack spotify ultimaker-cura vanilla) # TODO further separate these out into categories
 
 # non-brew packages
