8fc367b25246af692440a703d48013d0749e718b
konapun <bremen.braun@gmail.com>
2022-06-26 21:08:39 -0500

    fix deprecation

--- nvim/lua/config/diffview.lua
+++ nvim/lua/config/diffview.lua
@@ -3,8 +3,10 @@ local cb = require'diffview.config'.diffview_callback
 require('diffview').setup {
   diff_binaries = false,    -- Show diffs for binaries
   file_panel = {
-    width = 35,
-    -- use_icons = true        -- Requires nvim-web-devicons
+    win_config = {
+      width = 35
+    },
+    use_icons = true        -- Requires nvim-web-devicons
   },
   key_bindings = {
     disable_defaults = false,                   -- Disable the default key bindings
