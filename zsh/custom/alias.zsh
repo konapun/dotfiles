246493421a82a9c5e2544743500fc8a4fc331508
bremen.braun <bremen.braun@sezzle.com>
2022-07-19 11:22:52 -0500

    alias system dependent clipboard copy/paste commands

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -11,3 +11,8 @@ command -v lsd &> /dev/null && alias ls=lsd
 command -v xdg-open &> /dev/null && alias open=xdg-open
 command -v cmd.exe &> /dev/null && alias open='cmd.exe /C start'
 command -v thefuck &> /dev/null && eval $(thefuck --alias)
+command -v clipcopy &> /dev/null && alias copy=clipcopy # Linux
+command -v clippaste &> /dev/null && alias paste=clippaste # Linux
+command -v pbcopy &> /dev/null && alias copy=pbcopy # MacOS
+command -v pbpaste &> /dev/null && alias paste=pbpaste # MacOS
+command -v clip.exe &> /dev/null && alias copy=clip.exe # WSL
