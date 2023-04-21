762a58f903c8211029cc3f8ac41a13dd0165b82f
konapun <bremen.braun@gmail.com>
2023-04-21 12:54:26 -0500

    Add racket

--- packages/mac.zsh
+++ packages/mac.zsh
@@ -2,7 +2,7 @@ PACKAGE_MANAGER=brew
 GRAPHICAL_PACKAGE_MANAGER="brew --cask"
 INSTALL_COMMAND="install"
 
-CORE_PACKAGES=(bat calcurse clisp ctags fzf gh golang gron gsed highlight htop hub luarocks lsd navi neofetch neovim node ripgrep roswell rust thefuck tmux) # babashka docker fortune guile stow
+CORE_PACKAGES=(bat calcurse clisp ctags fzf gh golang gron gsed highlight htop hub luarocks lsd navi neofetch neovim node racket ripgrep roswell rust thefuck tmux) # babashka docker fortune guile stow
 GRAPHICAL_PACKAGES=(alacritty amethyst edex-ui font-cascadia-pl font-ibm-plex dash gimp iterm2 kitty prusaslicer raycast rectangle slack spotify vanilla) # TODO further separate these out into categories
 
 # non-brew packages
