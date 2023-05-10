9fb2ecefc5a473682959763c7209e05d1f7fc0c0
konapun <bremen.braun@gmail.com>
2023-05-09 22:26:46 -0500

    set relativenumber by default

--- nvim/custom/init.lua
+++ nvim/custom/init.lua
@@ -1,7 +1,9 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
+
+vim.wo.relativenumber = true
