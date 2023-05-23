813e8f79699a1018155b13bf4122645e4640e0d2
konapun <bremen.braun@gmail.com>
2023-05-22 21:35:50 -0500

    Change theme name

--- nvim/custom/themes/monochrome.lua
+++
@@ -1,8 +0,0 @@
-local base46 = require("base46-rt")
-local colors = require("custom.bridge").ui.theme.colors
-
-local base16_colors = base46.scheme.monochrome(colors.primary, 1)
-
-local theme = base46.theme("dark", base16_colors)
-
-return theme
