e1ede3d3f69b5e4f899296b98af7383feee94392
konapun <bremen.braun@gmail.com>
2023-08-21 22:57:08 -0500

    Add symbols outline

--- nvim/custom/mappings.lua
+++ nvim/custom/mappings.lua
@@ -2,57 +2,70 @@
 local M = {}
 
 M.general = {
-  n = {
-    ["<Esc>"] = { "<cmd> :noh <CR> :cclose <CR>", "clear highlights and close quickfix panel" },
-    ["<leader>ww"] = { "<cmd> set wrap! <CR>", "toggle word wrapping" },
-    ["<leader>nn"] = { "<cmd> set nu! <CR>", "toggle line number" },
-    ["<leader>nt"] = { "<cmd> tabnew <CR>", "new tab" },
-    ["<leader>nb"] = { "<cmd> enew <CR>", "new buffer" },
-  },
+	n = {
+		["<Esc>"] = { "<cmd> :noh <CR> :cclose <CR>", "clear highlights and close quickfix panel" },
+		["<leader>ww"] = { "<cmd> set wrap! <CR>", "toggle word wrapping" },
+		["<leader>nn"] = { "<cmd> set nu! <CR>", "toggle line number" },
+		["<leader>nt"] = { "<cmd> tabnew <CR>", "new tab" },
+		["<leader>nb"] = { "<cmd> enew <CR>", "new buffer" },
+		["<leader>s"] = { "<cmd> :SymbolsOutline <CR>", "toggle symbols outline" },
+	},
 }
 
 M.copilot = {
-  i = {
-    ["<C-\\>"] = { "<cmd>Copilot suggestion accept_line<CR>", "accept suggestion" },
-  },
-  n = {
-    ["<C-\\>"] = { "<cmd>Copilot panel open<CR>", "open panel" },
-  },
+	i = {
+		["<C-\\>"] = { "<cmd>Copilot suggestion accept_line<CR>", "accept suggestion" },
+	},
+	n = {
+		["<C-\\>"] = { "<cmd>Copilot panel open<CR>", "open panel" },
+	},
 }
 
 M.telescope = {
-  n = {
-    ["<leader>fs"] = { "<cmd> Telescope lsp_document_symbols <CR>", "find symbols (document)" },
-    ["<leader>fS"] = { "<cmd> Telescope lsp_workspace_symbols <CR>", "find symbols (workspace)" },
-    ["<leader>fd"] = { "<cmd> Telescope diagnostics bufnr=0 <CR>", "find diagnostics (current buffer)" },
-    ["<leader>fD"] = { "<cmd> Telescope diagnostics <CR>", "find diagnostics (all)" },
-    ["<leader>fj"] = { "<cmd> Telescope jumplist <CR>", "find jumplist" },
-    ["<leader>fr"] = { "<cmd> Telescope resume <CR>", "find resume" },
-  },
+	n = {
+		["<leader>fs"] = { "<cmd> Telescope lsp_document_symbols <CR>", "find symbols (document)" },
+		["<leader>fS"] = { "<cmd> Telescope lsp_workspace_symbols <CR>", "find symbols (workspace)" },
+		["<leader>fd"] = { "<cmd> Telescope diagnostics bufnr=0 <CR>", "find diagnostics (current buffer)" },
+		["<leader>fD"] = { "<cmd> Telescope diagnostics <CR>", "find diagnostics (all)" },
+		["<leader>fj"] = { "<cmd> Telescope jumplist <CR>", "find jumplist" },
+		["<leader>fr"] = { "<cmd> Telescope resume <CR>", "find resume" },
+	},
 }
 
 M.debug = {
-  n = {
-    ["<leader>dc"] = { "<cmd> lua require('dap').continue()<CR>", "continue" },
-    ["<leader>ds"] = { "<cmd> lua require('dap').step_over()<CR>", "step over" },
-    ["<leader>di"] = { "<cmd> lua require('dap').step_into()<CR>", "step into" },
-    ["<leader>do"] = { "<cmd> lua require('dap').step_out()<CR>", "step out" },
-    ["<leader>db"] = { "<cmd> lua require('dap').toggle_breakpoint()<CR>", "toggle breakpoint" },
-    ["<leader>dB"] = { "<cmd> lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", "set breakpoint" },
-    ["<leader>dl"] = { "<cmd> lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", "log point" },
-    ["<leader>dr"] = { "<cmd> lua require('dap').repl.open()<CR>", "open repl" },
-    ["<leader>de"] = { "<cmd> lua require('dap').repl.run_last()<CR>", "run last" },
-    ["<leader>dd"] = { "<cmd> lua require('dap').disconnect()<CR>", "disconnect" },
-    ["<leader>dk"] = { "<cmd> lua require('dap').close()<CR>", "close" },
-    ["<leader>df"] = { function()
-      local widgets = require('dap.ui.widgets')
-      widgets.centered_float(widgets.frames)
-    end, "view frames" },
-    ["<leader>dy"] = { function()
-      local widgets = require('dap.ui.widgets')
-      widgets.centered_float(widgets.scopes)
-    end, "view scopes"}
-  }
+	n = {
+		["<leader>dc"] = { "<cmd> lua require('dap').continue()<CR>", "continue" },
+		["<leader>ds"] = { "<cmd> lua require('dap').step_over()<CR>", "step over" },
+		["<leader>di"] = { "<cmd> lua require('dap').step_into()<CR>", "step into" },
+		["<leader>do"] = { "<cmd> lua require('dap').step_out()<CR>", "step out" },
+		["<leader>db"] = { "<cmd> lua require('dap').toggle_breakpoint()<CR>", "toggle breakpoint" },
+		["<leader>dB"] = {
+			"<cmd> lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+			"set breakpoint",
+		},
+		["<leader>dl"] = {
+			"<cmd> lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+			"log point",
+		},
+		["<leader>dr"] = { "<cmd> lua require('dap').repl.open()<CR>", "open repl" },
+		["<leader>de"] = { "<cmd> lua require('dap').repl.run_last()<CR>", "run last" },
+		["<leader>dd"] = { "<cmd> lua require('dap').disconnect()<CR>", "disconnect" },
+		["<leader>dk"] = { "<cmd> lua require('dap').close()<CR>", "close" },
+		["<leader>df"] = {
+			function()
+				local widgets = require("dap.ui.widgets")
+				widgets.centered_float(widgets.frames)
+			end,
+			"view frames",
+		},
+		["<leader>dy"] = {
+			function()
+				local widgets = require("dap.ui.widgets")
+				widgets.centered_float(widgets.scopes)
+			end,
+			"view scopes",
+		},
+	},
 }
 
 return M
--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -3,127 +3,135 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+	-- Override plugin definition options
 
-  -- utils
-  "konapun/base46-rt",
+	-- utils
+	"konapun/base46-rt",
 
-  -- plugins
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require("custom.configs.null-ls")
-        end,
-      },
-    },
-    config = function()
-      require("plugins.configs.lspconfig")
-      require("custom.configs.lspconfig")
-    end, -- Override to setup mason-lspconfig
-  },
+	-- plugins
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-    -- enabled = false,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+		-- enabled = false,
+	},
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
 
-  -- Custom plugins
+	-- Custom plugins
 
-  {
-    "ggandor/leap.nvim",
-    event = "BufEnter",
-    dependencies = {
-      "tpope/vim-repeat",
-    },
-    config = function()
-      require("leap").add_default_mappings()
-    end,
-  },
+	{
+		"ggandor/leap.nvim",
+		event = "BufEnter",
+		dependencies = {
+			"tpope/vim-repeat",
+		},
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+	},
 
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end,
-  },
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-        },
-      })
-    end,
-  },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+				},
+			})
+		end,
+	},
 
-  {
-    "mfussenegger/nvim-dap",
-    event = "VeryLazy",
-  },
+	{
+		"mfussenegger/nvim-dap",
+		event = "VeryLazy",
+	},
 
-  {
-    "stevearc/overseer.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("overseer").setup()
-    end
-  },
+	{
+		"stevearc/overseer.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("overseer").setup()
+		end,
+	},
 
-  {
-    "andythigpen/nvim-coverage",
-    event = "BufRead",
-    config = function()
-      require("coverage").setup()
-    end
-  },
+	{
+		"andythigpen/nvim-coverage",
+		event = "BufRead",
+		config = function()
+			require("coverage").setup()
+		end,
+	},
 
-  {
-    "chentoast/marks.nvim",
-    event = "BufRead",
-    config = function()
-      require("marks").setup()
-    end
-  },
+	{
+		"simrat39/symbols-outline.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("symbols-outline").setup()
+		end,
+	},
+
+	{
+		"chentoast/marks.nvim",
+		event = "BufRead",
+		config = function()
+			require("marks").setup()
+		end,
+	},
 }
 
 return plugins
