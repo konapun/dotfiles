90d3235249fc6e984d25d01cedfe527b849dc333
konapun <bremen.braun@gmail.com>
2024-02-28 09:43:23 -0600

    fixes for asdf

--- packages/postinstall/node.zsh
+++ packages/postinstall/node.zsh
@@ -3,5 +3,5 @@ for node_version in $NODE_VERSIONS; do
 done
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf install $NODE_VERSION
+asdf install nodejs $NODE_VERSION
 asdf global nodejs $NODE_VERSION
