e6addefc24066b3bb521d35d2590dce8f0ad2bdb
konapun <bremen.braun@gmail.com>
2024-03-29 12:37:38 -0500

    remove ctags pkg

--- packages/linux/package_manager/apk.zsh
+++ packages/linux/package_manager/apk.zsh
@@ -3,7 +3,7 @@ PACKAGE_MANAGER="apk"
 INSTALL_COMMAND="add"
 
 # missing: bat clojure gron hub leiningen thefuck
-CORE_PACKAGES=(bash calcurse cargo ctags command-not-found curl docker docker-compose fzf highlight htop neofetch neovim nodejs npm python3 py3-pip ripgrep tmux tree zsh) # stow navi ncmpcpp sxhkd
+CORE_PACKAGES=(bash calcurse cargo command-not-found curl docker docker-compose fzf highlight htop neofetch neovim nodejs npm python3 py3-pip ripgrep tmux tree zsh) # stow navi ncmpcpp sxhkd
 GRAPHICAL_PACKAGES=()
 
 # packages which aren't currently available in apt
