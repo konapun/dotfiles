7235e132ccec4739f7fae2fc5af533121b320637
konapun <bremen.braun@gmail.com>
2024-05-10 21:48:11 -0500

    Add zen mode plugin, formatting

--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -199,6 +199,30 @@ local plugins = {
 			})
 		end,
 	},
+
+	{
+		"folke/zen-mode.nvim",
+		opts = {
+			window = {
+				backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+				-- height and width can be:
+				-- * an absolute number of cells when > 1
+				-- * a percentage of the width / height of the editor when <= 1
+				-- * a function that returns the width or the height
+				width = 80, -- width of the Zen window
+				height = 1, -- height of the Zen window
+				options = {
+					signcolumn = "no", -- disable signcolumn
+					number = false, -- disable number column
+					relativenumber = false, -- disable relative numbers
+					cursorline = false, -- disable cursorline
+					cursorcolumn = false, -- disable cursor column
+					foldcolumn = "0", -- disable fold column
+					list = false, -- disable whitespace characters
+				},
+			},
+		},
+	},
 }
 
 return plugins
